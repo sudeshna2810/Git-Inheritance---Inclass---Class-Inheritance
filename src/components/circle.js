3259e43e9e7319eff77759f234a82cdb3a881282
abhishekshaw1007 <abhishekshaw1007@gmail.com>
2020-10-28 22:54:19 +0530

    Inheritance Code

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,30 @@
 // import using require
 
+
 // declare class
 
+// es6 default import syntax
+import Shape from './shape';
+
+// Circle Class extending Shape Class
+export default class Circle extends Shape {
+    constructor() {
+        // first line of subclass constructor should call superclass constructor with appropriate parameters
+        super();
+    }
+    // to overrride function in JS, redeclare the function in SubClass with same name as of Super Class
+    calculateArea() {
+        console.log("Area of Circle with color: " + this.color);// since Circle has extended Shape, we can 
+                                                                //access color variable of Shape here
+        return 3.14*5*5;
+    }
+}
+/*
+class GreenCircle extends Circle {
+    constructor() {
+        super("Green");
+    }
+}
+*/
 
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,30 @@
+/**
+Acceptance Criteria
+
+Create a base class called "Shape" with methods drawShape, calculateArea and color property that is initialised with a color.
+
+In another module make a class called "Circle" that extends the "Shape" class and overrides the calculateArea method.
+
+Write code for Shape class in shape.js and Circle in circle.js
+
+Export the class from each file.
+
+Note: Don't remove any file from the cloned repo, otherwise all test cases will fail. Use module.exports for export and require for import. If it is not covered in class, google it :)
+ */
+
 // declare class
+
+export default class Shape {  // es6 default export syntax
+    constructor() {
+        this.color = "blue";
+    }
+    drawShape() {
+        console.log("Drawing a Shape");
+    }
+    calculateArea() {
+        console.log("Area of unknow Shape");
+    }
+}
+
 
 // export class using module.exports
